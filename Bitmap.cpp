e9df4f9a58143f7cdc47963d48dbd1f498cacf33
Akil Hamilton <akil.james83@gmail.com>
2018-12-31 06:02:32 -0500

    Added Bitmap Class Files

---
+++ Bitmap.cpp
@@ -0,0 +1,17 @@
+#include "Bitmap.h"
+
+Bitmap::Bitmap (int width, int height) : m_width(width), m_height(height) {
+
+}
+
+bool Bitmap::write(string filename) {
+	return false;
+}
+
+void Bitmap::setPixel(int x, int y, uint8_t red, uint8_t green, uint8_t blue) {
+	return;
+}
+
+Bitmap::~Bitmap() {
+
+}
