ae2cf840ff3d2b3f6cdf254f520e13eef510af8a
Akil Hamilton <akil.james83@gmail.com>
2018-12-31 06:18:58 -0500

    Adding functionallity for bitmap file writing

--- BitmapFileHeader.h
+++ BitmapFileHeader.h
@@ -13,7 +13,7 @@ struct BitmapFileHeader {
 		For writing information to the bitmap file.
 	*/
 	char header[2]{'B','M'}; 
-	int32_t filesize;
+	int32_t fileSize;
 	int32_t reserved{0};
 	int32_t dataOffset;
 
