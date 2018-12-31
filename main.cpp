2a282d68253cc52aca55f7a4dcd546e5353d3499
Akil Hamilton <akil.james83@gmail.com>
2018-12-31 07:55:15 -0500

    Changes to algorithm to improve final output

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdint>
+#include <memory>
 #include "Bitmap.h"
 #include "Mandelbrot.h"
 
@@ -7,8 +8,8 @@ using namespace akiljames;
 using namespace std;
 
 int main() {
-	int const WIDTH = 400;
-	int const HEIGHT = 300;
+	int const WIDTH = 800;
+	int const HEIGHT = 600;
 	Bitmap bitmap(WIDTH, HEIGHT);
 
 	double min = 9999999;
@@ -16,15 +17,17 @@ int main() {
 
 	for (int w = 0; w < WIDTH; w++){
 		for (int h = 0; h < HEIGHT; h++){
-			double xFractal = ((double)w/WIDTH) * 2 - 1;
-			double yFractal = ((double)h/HEIGHT) * 2 - 1;
+			double xFractal =  (w - (WIDTH*1.38)/2) * 2./(HEIGHT*0.85); //((double)(w - 200)/WIDTH) * 2 - 1;
+			double yFractal = (h - HEIGHT/2) * 2./(HEIGHT*0.85); //((double)h/HEIGHT) * 2 - 1;
 
 			int iterations = Mandelbrot::getIterations(xFractal, yFractal);
-			uint8_t red = static_cast<uint8_t>(256 * static_cast<double>(iterations)/Mandelbrot::MAX_ITERATIONS);
+			uint8_t color = static_cast<uint8_t>(256 * static_cast<double>(iterations)/Mandelbrot::MAX_ITERATIONS);
 
-			bitmap.setPixel(w, h, red, red, red);
-			if(red < min) min = red;
-			if(red > max) max = red;
+			color = color * color * color * color; /* Play with this to change the way color is displayed */
+
+			bitmap.setPixel(w, h, color, 0, 0);
+			if(color < min) min = color;
+			if(color > max) max = color;
 			
 
 			
